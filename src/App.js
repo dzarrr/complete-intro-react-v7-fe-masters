c4952692060cd0b3200e7ef669925f8def4e97f2
dzarrr <dzarbelahanifa@gmail.com>
2022-07-31 09:26:52 +0700

    finish chapter 1: vanilla react

---
+++ src/App.js
@@ -0,0 +1,36 @@
+const Pet = (props) => {
+  return (
+    React.createElement('div', {}, [
+      React.createElement('h1', {}, props.name),
+      React.createElement('h2', {}, props.animal),
+      React.createElement('h2', {}, props.breed),
+    ])
+  )
+}
+
+const App = () => {
+  return React.createElement(
+    'div',
+    {},
+    [
+      React.createElement('h1', {}, 'Adopt Me!'),
+      React.createElement(Pet, {
+        name: 'Pompom',
+        animal: 'cat',
+        breed: 'kampung'
+      }),
+      React.createElement(Pet, {
+        name: 'Bili',
+        animal: 'cat',
+        breed: 'kampung'
+      }),
+      React.createElement(Pet, {
+        name: 'Aam',
+        animal: 'cat',
+        breed: 'kampung'
+      }),
+    ]
+  )
+}
+
+ReactDOM.render(React.createElement(App), document.getElementById('root'))
